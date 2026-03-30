d09a58690813138b91cd9a61eda1f803a86b9484
abahe <fathur.jtk09@gmail.com>
2026-03-30 20:09:32 +0700

    event bubbling

--- js_dom_wpu/dom_traversal/script.js
+++ js_dom_wpu/dom_traversal/script.js
@@ -1,17 +1,30 @@
 
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 
-close.forEach(function(el) {
-    el.addEventListener('click', function(e){
+// close.forEach(function(el) {
+//     el.addEventListener('click', function(e){
+//         e.target.parentElement.style.display = 'none';
+//         e.preventDefault();
+//         e.stopPropagation();
+//     });
+// });
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function(card) {
+//     card.addEventListener('click', function(e) {
+//         alert('ok');
+//     });
+// });
+
+
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(e) {
+    //console.info("yo yo");
+    console.info(e.target);
+    if( e.target.className == 'close') {
         e.target.parentElement.style.display = 'none';
         e.preventDefault();
-        e.stopPropagation();
-    });
-});
-
-const cards = document.querySelectorAll('.card');
-cards.forEach(function(card) {
-    card.addEventListener('click', function(e) {
-        alert('ok');
-    });
+    }
 });
