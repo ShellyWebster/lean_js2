105a6fa3bf7a05d4bb5646483d7b99d54c494f3b
abahe <fathur.jtk09@gmail.com>
2026-04-09 20:35:52 +0700

    otw node

---
+++ wpu_js_lanjutan/fetch/script.js
@@ -0,0 +1,86 @@
+// $('.search-button').on('click', function() {
+
+//     $.ajax({
+//         // url: 'x',
+//         url: 'https://www.omdbapi.com/?apikey=640e288c&s=' + $('.input-keyword').val(),
+//         success: result => {
+//             const movies = result.Search;
+//             // console.log(movies);
+//             let cards = '';
+//             movies.forEach(m => {
+//                 cards += showCards(m);
+//             });
+//             $('.movie-container').html(cards)
+
+//             // ketika tombol detail di-klik 
+//             $('.modal-detail-button').on('click', function(){
+//                 console.log($(this).data('imdbid'));
+                
+//                 $.ajax({
+//                     url: 'https://www.omdbapi.com/?apikey=640e288c&i=' + $(this).data('imdbid'),
+//                     success: m => {
+//                         console.log(m.Title);
+//                         const movieDetail = showMovieDetail(m);
+//                                             $('.modal-body').html(movieDetail);
+//                     },
+//                     error: (e) => {
+//                         console.log(e.responseText);
+//                     }
+
+//                 });
+//             });
+//         },
+//         error: (e) => {
+//             console.log(e.responseText);
+//         }
+//     });
+
+// });
+
+
+
+//fetch 
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function() {
+
+    const intputKeyword = document.querySelector('.input-keyword');
+    fetch('https://www.omdbapi.com/?apikey=640e288c&s=' + intputKeyword.value)
+        .then(response => response.json())
+        .then(response => console.log(response));
+});
+
+
+
+function showMovieDetail(m) {
+    return `<div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <img src="${m.Poster}" class="img-fluid">
+                                                </div>
+                                                <div class="col-md">
+                                                    <ul class="list-group">
+                                                        <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                                                        <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
+                                                        <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
+                                                        <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}</li>
+                                                        <li class="list-group-item"><strong>Plot : </strong> ${m.Plot}</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>`;
+}
+
+
+function showCards(m) {
+    return `<div class="col-md-4 my-3">
+                        <div class="card">
+                            <img class="card-img-top" src="${m.Poster}">
+                            <div class="card-body">
+                                <h5 class="card-title">${m.Title}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                                <a href="#" class="btn btn-primary modal-detail-button" 
+                                data-imdbid="${m.imdbID}" data-bs-toggle="modal" data-bs-target="#movieDetailModal">Show Details</a>
+                            </div>
+                        </div>
+                    </div>`;
+}
